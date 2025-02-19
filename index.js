13e626c39b286c66607984701c2604e9fd1242ad
shivendra8009727256 <monuy8009727256@gmail.com>
2025-02-19 21:20:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ const port=process.env.port;
 
 // Start the server
 app.listen(8080, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:8080`);
 });
